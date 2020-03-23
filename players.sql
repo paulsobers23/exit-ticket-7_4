dc81453a98abb3f3e1526c7641f6c530aa50efeb
paulsobers23 <ec2-user@ip-172-31-19-228.us-east-2.compute.internal>
2020-03-23 21:48:28 +0000

    finished but for some reason UNIQUE is not working for me on aws or serial

---
+++ players.sql
@@ -0,0 +1,29 @@
+CREATE TABLE players(
+id integer UNIQUE,
+last_name text,
+first_name text,
+position text ,
+jersery_number integer,
+weight_lb integer,
+height_in integer,
+date_of_birth date
+);
+
+INSERT INTO players VALUES(1,'Fisher','Derek','QB',2,210,74,'January 2,1999');
+INSERT INTO players VALUES(2,'Cunningham','Reginald','DE',55,240,75,'April 3,1998');
+INSERT INTO players VALUES(3,'Jackson', 'Stephen','WR',13,184,76,'October 13, 1998');
+INSERT INTO players VALUES(4,'Colton','Ryan','K',81,238,74,'December 12,1998');
+INSERT INTO players VALUES(5,'Wright','Alexander','RB',23,190,70,'March 13,1998');
+INSERT INTO players VALUES(6,'Smalls','Elliot','OLB',33,215,69,'February ,1998');
+
+ALTER TABLE players
+ALTER COLUMN position SET DATA TYPE VARCHAR(3), 
+ALTER COLUMN position SET NOT NULL,
+ALTER COLUMN position SET DEFAULT 'AHT',
+ADD CHECK (jersery_number > 1 AND jersery_number < 100), 
+ALTER COLUMN jersery_number SET NOT NULL;
+
+ALTER TABLE players ADD CONSTRAINT id UNIQUE;
+
+INSERT INTO players VALUES(7,'Jamison','Zion','AHT',12,285,80,'1999-11-25');
+INSERT INTO players VALUES(8,'Johnson','Craig','P',17,173,72);
